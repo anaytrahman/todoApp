bc16bdf37d42ba5a9d4ef397e3ed9ae7b3ab2d65
Hidayt Rahman <hidaytrahman@gmail.com>
2020-09-12 17:14:26 +0530

    added storage feature

--- js/script.js
+++ js/script.js
@@ -4,23 +4,43 @@ var $saveBtn = document.querySelector("#save-btn");
 var $todoList = document.querySelector("#todo-list");
 var $editTodo = document.querySelector("#edit-todo");
 
+// get browser todo value in the variable
+var todoFromStorage = localStorage.getItem("todoListArr");
+
 //for user value title and description
 var todoList = [];
+
+var arrayTodo = [];
+// check if browser variable or value is exist
+if(todoFromStorage) {
+  console.log('is exist');
+  // convert it into array from strng
+  arrayTodo = JSON.parse(todoFromStorage);
+}
+
+// check if browser has todo list
+if(arrayTodo.length > 0) {
+  // set all todo from browser to main array
+  todoList = arrayTodo;
+  // show dom on load
+  showDOMList();
+}
+
 
 //btn to handle create
 $saveBtn.addEventListener("click", function () {
   createTodo();
 });
 
-window.addEventListener("keyup", function(e) {
+window.addEventListener("keyup", function (e) {
   var currentKey = e.key;
 
   // check enter
-  if(currentKey.toLowerCase() === "enter") {
+  if (currentKey.toLowerCase() === "enter") {
     createTodo();
   }
 
-  if(e.key === "Escape") {
+  if (e.key === "Escape") {
     window.close();
   }
 });
@@ -30,7 +50,7 @@ window.addEventListener("keyup", function(e) {
 function createTodo() {
   var titleVal = $todoTitle.value;
   var descrpVal = $todoDescrp.value;
-  
+
   // event entered multiple space will be removed because of trim() method
   if (titleVal.trim() != "") {
     todoList.push({
@@ -39,7 +59,7 @@ function createTodo() {
     });
 
     showDOMList();
-    
+
     // reset value and make focus on title again
     $todoTitle.value = "";
     $todoDescrp.value = "";
@@ -54,19 +74,19 @@ function showDOMList() {
   console.log('todo', todoList);
   // reset element on every click
   $todoList.innerHTML = "";
- 
-  
-  for(var i=0; i < todoList.length; i++) {
 
-  var iconEdit = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+
+  for (var i = 0; i < todoList.length; i++) {
+
+    var iconEdit = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
 </svg>`;
-var iconDelete = `
+    var iconDelete = `
 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
 </svg>`;
 
-  var listDOM = `
+    var listDOM = `
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">${todoList[i].title}</h5>
@@ -92,7 +112,7 @@ var iconDelete = `
       listDOM
     );
   }
-  
+
   //store data
   storeTodo();
 }
@@ -102,17 +122,17 @@ function deleteTodo(index) {
   // show confirm box
   var deleteConfirmation = confirm("Are you sure?");
   // if yes delete or if no dont delete
-  if(deleteConfirmation) {
-      // delete item from the todo array
-      todoList.splice(index,1);
-      showDOMList();
-      
-      // todo: will show a good looking notification message
+  if (deleteConfirmation) {
+    // delete item from the todo array
+    todoList.splice(index, 1);
+    showDOMList();
+
+    // todo: will show a good looking notification message
   }
- }
+}
 
 
- // get and set update value
+// get and set update value
 function getTodoForUpdate(index, title, descrp) {
   var $editTitle = $editTodo.querySelector("#todo-title-edit");
   var $editDescrp = $editTodo.querySelector("#todo-descrp-edit");
@@ -122,7 +142,7 @@ function getTodoForUpdate(index, title, descrp) {
   $editDescrp.value = descrp;
 
   // fire update todo method on click
-  $btnUpdateTodo.onclick =  function() {
+  $btnUpdateTodo.onclick = function () {
     var updatedTitle = $editTitle.value;
     var updatedDescrp = $editDescrp.value;
 
@@ -136,9 +156,9 @@ function updateTodo(index, updatedTitle, updatedDescrp) {
 
   // update item from the todo array
   todoList.splice(index, 1, {
-      title: updatedTitle,
-      descrp: updatedDescrp
-    }
+    title: updatedTitle,
+    descrp: updatedDescrp
+  }
   );
 
   showDOMList();
